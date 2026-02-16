66e9f6f027f1b4baa85c6504f73be6a9860cbb14
Anu-A5 <113447677+Anu-A5@users.noreply.github.com>
2026-02-16 13:35:27 +0000

    Add files via upload

---
+++ src/main.c
@@ -0,0 +1,273 @@
+/*
+ * Copyright (c) 2018 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
+ */
+
+#include <zephyr/types.h>
+#include <stddef.h>
+#include <string.h>
+#include <errno.h>
+#include <zephyr/sys/printk.h>
+#include <zephyr/sys/byteorder.h>
+#include <zephyr/kernel.h>
+#include <zephyr/drivers/gpio.h>
+#include <soc.h>
+
+#include <zephyr/bluetooth/bluetooth.h>
+#include <zephyr/bluetooth/hci.h>
+#include <zephyr/bluetooth/conn.h>
+#include <zephyr/bluetooth/uuid.h>
+#include <zephyr/bluetooth/gatt.h>
+
+#include <bluetooth/services/lbs.h>
+
+#include <zephyr/settings/settings.h>
+
+#include <dk_buttons_and_leds.h>
+
+#include <zephyr/drivers/i2c.h>
+
+#define DEVICE_NAME             CONFIG_BT_DEVICE_NAME
+#define DEVICE_NAME_LEN         (sizeof(DEVICE_NAME) - 1)
+
+
+#define RUN_STATUS_LED          DK_LED1
+#define CON_STATUS_LED          DK_LED2
+#define RUN_LED_BLINK_INTERVAL  1000
+
+#define USER_LED                DK_LED3
+
+#define USER_BUTTON             DK_BTN1_MSK
+
+/* GDP Related Params */
+
+//i2c
+
+#define BT_POLLING_RATE     1000 //send data over bt every 1 second
+
+#define PPG_POLLING_RATE	40
+#define MIC_POLLING_RATE    40
+#define PPG_BUFFER_SIZE     BT_POLLING_RATE/PPG_POLLING_RATE
+#define MIC_BUFFER_SIZE     BT_POLLING_RATE/MIC_POLLING_RATE
+
+#define I2C1_PPG_NODE DT_NODELABEL(ppg);
+#define I2C1_MIC_NODE DT_NODELABEL(mic);
+
+//BT
+static K_SEM_DEFINE(ble_init_ok, 0, 1);
+
+
+static bool app_button_state;
+
+//Get i2c bus pointer and target address
+
+static const struct i2c_dt_spec ppg_i2c = I2C_DT_SPEC_GET(I2C1_PPG_NODE);
+static const struct i2c_dt_spec mic_i2c = I2C_DT_SPEC_GET(I2C1_MIC_NODE);
+
+//Declare FIFOs for I2C Data 
+//A FIFO is a kernel object that implements a traditional first in, first out (FIFO) queue, allowing threads and ISRs to add and remove data items of any size.
+static K_FIFO_DEFINE(ppg_data_fifo); 
+static K_FIFO_DEFINE(mic_data_fifo);
+
+//Declare the struct of the data item of the FIFOs
+struct ppg_data_t {
+	void *fifo_reserved;
+	int32_t data[PPG_BUFFER_SIZE];
+	uint16_t len;
+};
+
+struct mic_data_t {
+	void *fifo_reserved;
+	int32_t data[MIC_BUFFER_SIZE];
+	uint16_t len;
+};
+
+static void i2c_cb()
+{
+	k_fifo_put(&ppg_data_fifo, )
+}
+
+//Define Thread to send data over bluetooth
+void ble_write_thread(void)
+{
+	/* Don't go any further until BLE is initialized */
+	k_sem_take(&ble_init_ok, K_FOREVER);
+	struct uart_data_t nus_data = {
+		.len = 0,
+	};
+
+	for (;;) {
+		/* Wait indefinitely for data to be sent over bluetooth */
+		struct uart_data_t *buf = k_fifo_get(&ppg_data_fifo, K_FOREVER);
+
+		int plen = MIN(sizeof(nus_data.data) - nus_data.len, buf->len);
+		int loc = 0;
+
+		while (plen > 0) {
+			memcpy(&nus_data.data[nus_data.len], &buf->data[loc], plen);
+			nus_data.len += plen;
+			loc += plen;
+
+			if (nus_data.len >= sizeof(nus_data.data) ||
+			    (nus_data.data[nus_data.len - 1] == '\n') ||
+			    (nus_data.data[nus_data.len - 1] == '\r')) {
+				if (bt_nus_send(NULL, nus_data.data, nus_data.len)) {
+					LOG_WRN("Failed to send data over BLE connection");
+				}
+				nus_data.len = 0;
+			}
+
+			plen = MIN(sizeof(nus_data.data), buf->len - loc);
+		}
+
+		k_free(buf);
+	}
+}
+
+K_THREAD_DEFINE(ble_write_thread_id, STACKSIZE, ble_write_thread, NULL, NULL, NULL, PRIORITY, 0, 0);
+
+// Generate random PPG data - Later should be connected to real PPG sensor
+
+// Generate random Microphone data
+
+// Method to process audio bluetooth data for the codec
+
+
+/* Bluetooth */
+static const struct bt_data ad[] = {
+	BT_DATA_BYTES(BT_DATA_FLAGS, (BT_LE_AD_GENERAL | BT_LE_AD_NO_BREDR)),
+	BT_DATA(BT_DATA_NAME_COMPLETE, DEVICE_NAME, DEVICE_NAME_LEN),
+};
+
+static const struct bt_data sd[] = {
+	BT_DATA_BYTES(BT_DATA_UUID128_ALL, BT_UUID_LBS_VAL),
+};
+
+static void connected(struct bt_conn *conn, uint8_t err)
+{
+	if (err) {
+		printk("Connection failed, err 0x%02x %s\n", err, bt_hci_err_to_str(err));
+		return;
+	}
+
+	printk("Connected\n");
+
+	dk_set_led_on(CON_STATUS_LED); //If connected, set led 2 on
+}
+
+static void disconnected(struct bt_conn *conn, uint8_t reason)
+{
+	printk("Disconnected, reason 0x%02x %s\n", reason, bt_hci_err_to_str(reason));
+
+	dk_set_led_off(CON_STATUS_LED); //If connected, set led 2 off
+}
+
+BT_CONN_CB_DEFINE(conn_callbacks) = {
+	.connected        = connected,
+	.disconnected     = disconnected,
+};
+
+static struct bt_conn_auth_cb conn_auth_callbacks;
+static struct bt_conn_auth_info_cb conn_auth_info_callbacks;
+
+
+static void app_led_cb(bool led_state)
+{
+	dk_set_led(USER_LED, led_state);
+}
+
+static bool app_button_cb(void)
+{
+	return app_button_state;
+}
+
+static struct bt_lbs_cb lbs_callbacs = {
+	.led_cb    = app_led_cb,
+	.button_cb = app_button_cb,
+};
+
+static void button_changed(uint32_t button_state, uint32_t has_changed)
+{
+	if (has_changed & USER_BUTTON) {
+		uint32_t user_button_state = button_state & USER_BUTTON;
+
+		bt_lbs_send_button_state(user_button_state);
+		app_button_state = user_button_state ? true : false;
+	}
+}
+
+static int init_button(void)
+{
+	int err;
+
+	err = dk_buttons_init(button_changed);
+	if (err) {
+		printk("Cannot init buttons (err: %d)\n", err);
+	}
+
+	return err;
+}
+
+int main(void)
+{
+	int blink_status = 0;
+	int err;
+
+	printk("Starting Bluetooth Peripheral LBS example\n");
+
+	err = dk_leds_init();
+	if (err) {
+		printk("LEDs init failed (err %d)\n", err);
+		return 0;
+	}
+
+	err = init_button();
+	if (err) {
+		printk("Button init failed (err %d)\n", err);
+		return 0;
+	}
+
+
+	/* GDP */
+	//Check if i2c bus is working
+	if (!device_is_ready(dev_i2c.bus)) {
+		printk("I2C bus %s is not ready!\n\r",dev_i2c.bus->name);
+		return;
+	}
+
+
+	err = bt_enable(NULL);
+	if (err) {
+		printk("Bluetooth init failed (err %d)\n", err);
+		return 0;
+	}
+
+	printk("Bluetooth initialized\n");
+
+	k_sem_give(&ble_init_ok);
+
+	if (IS_ENABLED(CONFIG_SETTINGS)) {
+		settings_load();
+	}
+
+	err = bt_lbs_init(&lbs_callbacs);
+	if (err) {
+		printk("Failed to init LBS (err:%d)\n", err);
+		return 0;
+	}
+
+	err = bt_le_adv_start(BT_LE_ADV_CONN, ad, ARRAY_SIZE(ad),
+			      sd, ARRAY_SIZE(sd));
+	if (err) {
+		printk("Advertising failed to start (err %d)\n", err);
+		return 0;
+	}
+
+	printk("Advertising successfully started\n");
+
+	for (;;) {
+		dk_set_led(RUN_STATUS_LED, (++blink_status) % 2);
+		k_sleep(K_MSEC(RUN_LED_BLINK_INTERVAL));
+	}
+}
